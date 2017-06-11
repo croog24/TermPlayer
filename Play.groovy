3c0d10a20a4a7b1827b826c0adfd0eaf38d29883
croog24 <croog24@gmail.com>
2017-06-11 14:31:51 -0400

    Play that Bach

--- Play.groovy
+++ Play.groovy
@@ -7,14 +7,15 @@ if (!radioStationFile.exists()) {
 def stationMap = [:]
 String currStation
 
+// Initialize station file
 radioStationFile.eachLine{line ->
 	if (line.startsWith('//')) {
 		return
 	}
  
 	if (line.startsWith('#')) {
-		currStation = line.substring(1);
-		stationMap.put(line.substring(1), new ArrayList<String>())
+		currStation = line.substring(1).trim()
+		stationMap.put(currStation, new ArrayList<String>())
 	} else if (line.trim().length() != 0) {
 		stationMap.get(currStation).add(line)
 	}
@@ -23,15 +24,36 @@ radioStationFile.eachLine{line ->
 def cli = new CliBuilder(usage: 'Play.groovy -[command]')
 cli.with {
 	l longOpt: 'list', 'List available stations'
+	p longOpt: 'play', 'Play the specified station', args : 1
+	h longOpt: 'help', 'Display usage information'
 }
 
 def options = cli.parse(args)
 
-if (!options) {
+if (!options || options.h) {
 	cli.usage()
 	return
 }
 
 if (options.l) {
+	printStations(stationMap)
+}
+
+if (options.p) {
+	if (!stationMap.containsKey(options.p)) {
+		println("Did not recognize the specified station: ${options.p}")
+		printStations(stationMap)		
+		System.exit(1)
+	}
+
+	def sout = new StringBuilder() 
+	def serr = new StringBuilder()
+	String station = stationMap.get(options.p).get(0)
+	String command = "/usr/bin/mpg123"
+	def process = [command, station]
+	process.execute()
+}
+
+void printStations(stationMap) {
 	stationMap.each{k, v -> println "${k}"}
 }
